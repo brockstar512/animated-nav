c7057ffbac129952c9b90cc1619532817402434b
marshall brock <marshallbrock@gmail.com>
2020-06-05 22:21:23 -0500

    fixed and put icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 
 function App() {
+  
   return (
     <div className="App">
       <Navbar>
-      <NavItem icon ="A"/>
-      <NavItem icon ="B"/>
-      <NavItem icon ="C"/>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
       </Navbar>
     </div>
   );
@@ -22,11 +33,36 @@ function Navbar(props){
   )
 }
 
+function DropdownMenu(){
+
+  function DropdownItem(props){
+    return(
+      <a href ="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    )
+  }
+  return(
+    <div className ="dropDown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem
+      leftIcon={<CogIcon/>}
+      rightIcon={<ChevronIcon/>}>
+      </DropdownItem>
+
+    </div>
+  )
+}
+
 function NavItem(props){
+  const [open, setOpen] = useState(false);
   return (<li className = "nav-item">
-    <a href="#" className="icon-button">
+    <a href="#" className="icon-button" onClick={()=> setOpen(!open)}>
       {props.icon}
       </a>
+      {open && props.children}
       </li>)
 }
 
